65cfaa378f92cad6905cfc1b415d523fabf26ed7
janaka <bjchathuranga@gmail.com>
2015-12-17 03:04:38 +0530

    Unit testing for ai

--- TestProject/AITesting.cs
+++ TestProject/AITesting.cs
@@ -5,15 +5,18 @@ using GameClientXNA;
 namespace TestProject
 {
     [TestClass]
-    public class UnitTest1
+    public class AITesting
     {
         [TestMethod]
-        public void Test_G()
+        public void Test_GetPath_Method()
         {
+
             GameDetail game = new GameDetail();
-            String msg = "G:P1;0,0;0;0;100;0;0:8,6,0;4,8,0;9,3,0;3,1,0;5,7,0#";
-            game.processMsg(msg,new TimeSpan(0));
-            //Assert.AreEqual(game.players[1].name, "P1");
+            //game.processMsg();
+
+
+
+            Assert.AreEqual(0, 0);
             //Assert.AreEqual(game.,)
         }
     }
